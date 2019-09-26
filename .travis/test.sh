f342a9e121059b734af8bf0ce6430c60afbaa09c
Egor Tensin <Egor.Tensin@gmail.com>
2019-09-26 14:39:11 +0300

    Travis: restructure test scripts

---
+++ .travis/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2019 Egor Tensin <Egor.Tensin@gmail.com>
+# This file is part of the "VK scripts" project.
+# For details, see https://github.com/egor-tensin/vk-scripts.
+# Distributed under the MIT License.
+
+./.travis/test/track_status.sh
+./.travis/test/online_sessions.sh
--- .travis/test/online_sessions.sh
+++ .travis/test/online_sessions.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2019 Egor Tensin <Egor.Tensin@gmail.com>
+# This file is part of the "VK scripts" project.
+# For details, see https://github.com/egor-tensin/vk-scripts.
+# Distributed under the MIT License.
+
 set -o errexit -o nounset -o pipefail
 
 readonly db_path='.travis/test_db.csv'
