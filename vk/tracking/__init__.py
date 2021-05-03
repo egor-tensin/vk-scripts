2870c59396c2d5787119cdadc002f760ae94b3fc
Egor Tensin <Egor.Tensin@gmail.com>
2021-05-03 21:36:46 +0300

    vk.tracking: merge online_sessions into sessions

--- vk/tracking/__init__.py
+++ vk/tracking/__init__.py
@@ -2,7 +2,6 @@
 # This file is licensed under the terms of the MIT License.
 # See LICENSE.txt for details.
 
-from .online_sessions import OnlineSessionEnumerator, Weekday
 from .status_tracker import StatusTracker
 
-__all__ = 'online_sessions', 'status_tracker',
+__all__ = 'sessions', 'status_tracker',
